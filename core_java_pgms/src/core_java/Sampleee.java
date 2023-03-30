427b0f240aad0452529b25022fb5b6d6c3a3deca
Moin Ahamed <Moin Ahamed@SMTC-R-0203>
2023-03-30 11:58:10 +0530

    new

---
+++ core_java_pgms/src/core_java/Sampleee.java
@@ -0,0 +1,10 @@
+package core_java;
+
+public class Sampleee {
+
+	public static void main(String[] args) {
+		System.out.println("hey ");
+
+	}
+
+}
---
+++ core_java_pgms/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Moin Ahamed
+ *
+ */
+module core_java_pgms {
+}
